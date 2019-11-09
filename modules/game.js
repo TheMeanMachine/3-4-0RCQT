660ab72827ed5616a666c5685e350c1d07db3e31
machina2 <machina2@coventry.ac.uk>
2019-11-09 14:40:28 +0000

    Made addNewGame()

--- modules/game.js
+++ modules/game.js
@@ -4,10 +4,14 @@
 const mime = require('mime-types')
 const sqlite = require('sqlite-async')
 
+//Custom modules
+const valid = require('./validator');
 
-module.exports = class game {
+
+module.exports = class Game {
 
 	constructor(dbName) {
+        this.validator = new valid();
 
 		return (async() => {
             this.dbName = dbName || ':memory:';
@@ -27,17 +31,33 @@ module.exports = class game {
 
     async addNewGame(title, summary, desc){
         try{
+            
+            let checkTitle = this.validator.check_MultipleWordsOnlyAlphaNumberic(title);
+            if(!checkTitle){
+                throw new Error('Must supply title');
+            }
+            let checkSummary = this.validator.check_MultipleWordsOnlyAlphaNumberic(summary);
+            if(!checkSummary){
+                throw new Error('Must supply summary');
+            }
+            let checkDesc = this.validator.check_MultipleWordsOnlyAlphaNumberic(desc);
+            if(!checkDesc){
+                throw new Error('Must supply description');
+            }
+
+            
+
             let sql = `SELECT COUNT(id) as records FROM game WHERE title="${title}";`;
 			const data = await this.db.get(sql);
 			if(data.records !== 0){
                 throw new Error(`Game "${title}" already exists`);
             }
 
-            let sql = `INSERT INTO game (title, summary, desc)
+            sql = `INSERT INTO game (title, summary, desc)
             VALUES(
-                ${title},
-                ${summary},
-                ${desc}
+                "${title}",
+                "${summary}",
+                "${desc}"
             )`;
             await this.db.run(sql);
 			return true;
