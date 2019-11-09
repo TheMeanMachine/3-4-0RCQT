0ff5dd57172103cedc037bcc5ebdffe9e2f72044
machina2 <machina2@coventry.ac.uk>
2019-11-09 13:26:09 +0000

    Added add game tests

--- unit tests/game.spec.js
+++ unit tests/game.spec.js
@@ -1,23 +1,58 @@
 
 'use strict'
 
-const Game = require('../modules/game');
+const Games = require('../modules/game.js');
 
-describe('addNewGame()', ()=>{
+describe('addNewGame()', () => {
     test('add a valid game', async done => {
         expect.assertions(1);
-        try{
-            const game = await new Game();
-            const addNewGame = await game.addNewGame('Red', "A simple summary", "Lorem Ipsum and as such this is a game");
-
-        }catch(e){
-            
-        }
         
-        expect(addNewGame).toBe(true);
+        const game = await new Games();
+        const newGame = await game.addNewGame("Red", "A simple summary", "Lorem Ipsum and as such this is a game");
+        expect(newGame).toBe(true);
     
 		done();
-	})
-	
+    })
+    
+    test('add a duplicate game', async done =>{
+        expect.assertions(1);
+        
+        const game = await new Games();
+        await game.addNewGame("Red", "A simple summary", "Lorem Ipsum and as such this is a game");
+        await expect(game.addNewGame("Red", "A simple summary", "Lorem Ipsum and as such this is a game"))
+            .rejects.toEqual(Error('Game "Red" already exists'));
+    
+		done();
+    })
+    
+    test('error if title empty', async done =>{
+        expect.assertions(1);
+
+        const game = await new Games();
+        await expect(game.addNewGame('', 'Summary', 'Description'))
+            .rejects.toEqual(Error('Must supply title'));
+
+        done();
+    })
+
+    test('error if summary empty', async done =>{
+        expect.assertions(1);
+
+        const game = await new Games();
+        await expect(game.addNewGame('Title', '', 'Description'))
+            .rejects.toEqual(Error('Must supply summary'));
+
+        done();
+    })
+
+    test('error if description empty', async done =>{
+        expect.assertions(1);
+
+        const game = await new Games();
+        await expect(game.addNewGame('Title', 'Summary', ''))
+            .rejects.toEqual(Error('Must supply description'));
+
+        done();
+    })
 })
 
