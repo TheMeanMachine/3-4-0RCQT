ec5827502010607c2285b3de7d83dc65dcd1ea52
machina2 <machina2@coventry.ac.uk>
2019-11-12 16:33:10 +0000

    Added checkReviewFields Added addReview

--- modules/review.js
+++ modules/review.js
@@ -1,20 +1,22 @@
 
 'use string'
 
+const sqlite = require('sqlite-async');
 //Custom modules
 const valid = require('./validator');
-const sqlite = require('sqlite-async');
+const Games = require('./game');
 
 module.exports = class Review {
-    constructor(dbName, router){
+    constructor(dbName){
         this.validator = new valid();
-        this.router = router;
+        
 		return (async() => {
+            
             this.dbName = dbName || ':memory:';
-			this.db = await sqlite.open(this.dbName);
+            this.db = await sqlite.open(this.dbName);
+            
             const sql = 
             [`
-    
             CREATE TABLE IF NOT EXISTS reviewScreenshot(
                 ID INTEGER PRIMARY KEY AUTOINCREMENT,
                 reviewID INTEGER,
@@ -32,6 +34,7 @@ module.exports = class Review {
                 FOREIGN KEY (gameID) REFERENCES game(ID),
                 FOREIGN KEY (userID) REFERENCES user(ID)
             );`]
+
             
             for(let i = 0; i < sql.length; i++){
                 await this.db.run(sql[i]);
@@ -41,8 +44,46 @@ module.exports = class Review {
 		})()
     }
 
-    
+    checkReviewFields(fullText, rating){
+        if(fullText != null){
+            let checkFullText = this.validator.check_MultipleWordsOnlyAlphaNumberic(fullText);
+            if(!checkFullText){
+                throw new Error('Must supply fulltext');
+            }
+        }
+        if(rating != null){
+            
+            if(isNaN(rating)){
+                throw new Error('Must supply rating')
+            
+            }
+            let greater = rating > 0;
+            let lesser = rating <= 5;
+            if(!greater || !lesser){
+                
+                throw new Error('Rating must be 1-5');
+            } 
+        }
+    }
 
-    
-    
+    async addReview(gameID, fullText, rating){
+        try{
+            if(gameID == null || isNaN(gameID)){
+                throw new Error('Must supply gameID');
+            }
+            
+            this.checkReviewFields(fullText, rating);
+            
+
+            let sql = `SELECT COUNT(ID) as records FROM game WHERE ID=${gameID};`;
+            const data = await this.db.get(sql);
+            if(data.records === 0){
+                throw new Error(`Game does not exist`);
+            }
+        }catch(e){
+            throw e;
+        }
+
+        return true;
+    }
 }
