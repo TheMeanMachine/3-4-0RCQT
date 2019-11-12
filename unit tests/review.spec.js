4496e2d5a936ba3dd79e5f077cd79da42922a9de
machina2 <machina2@coventry.ac.uk>
2019-11-12 16:32:35 +0000

    Edits to unit tests

--- unit tests/review.spec.js
+++ unit tests/review.spec.js
@@ -1,17 +1,19 @@
 'use strict'
-
+const sqlite = require('sqlite-async');
 const Reviews = require('../modules/review.js');
 const Games = require('../modules/game.js');
 
 describe('addReview()', ()=>{
     test('Valid review', async done =>{
-        expect.assertions(2);
+        expect.assertions(1);
 
-        const game = await new Games();
+        
+        const review = await new Reviews(null, db);
+        const game = await new Games(null, db);
 
         await game.addNewGame("title", "summary", "desc");
         const retreiveGame = await game.getGameByTitle("title");
-        const result = await addReview(retreiveGame.ID, "Full text", 4);
+        const result = await review.addReview(retreiveGame.ID, "Full text", 4);
 
         expect(result).toBe(true);
 
@@ -19,53 +21,81 @@ describe('addReview()', ()=>{
     })
 
     test('Error if: Invalid review _ fullText empty', async done =>{
-        expect.assertions(2);
+        expect.assertions(1);
 
         const game = await new Games();
-
+        const review = await new Reviews();
         await game.addNewGame("title", "summary", "desc");
         const retreiveGame = await game.getGameByTitle("title");
-        await expect(addReview(retreiveGame.ID, "", 4))
-            .rejects.toEqual(new Error('Must supply full text'));
+        await expect(review.addReview(retreiveGame.ID, "", 4))
+            .rejects.toEqual(Error('Must supply fulltext'));
         
         done();
     })
 
     test('Error if: Invalid review _ rating too high', async done =>{
-        expect.assertions(2);
+        expect.assertions(1);
 
         const game = await new Games();
-
+        const review = await new Reviews();
         await game.addNewGame("title", "summary", "desc");
         const retreiveGame = await game.getGameByTitle("title");
-        await expect(addReview(retreiveGame.ID, "Full text", 6))
-            .rejects.toEqual(new Error('Rating must be 1-5'));
+        await expect(review.addReview(retreiveGame.ID, "Full text", 6))
+            .rejects.toEqual(Error('Rating must be 1-5'));
         
         done();
     })
 
     test('Error if: Invalid review _ rating too low', async done =>{
-        expect.assertions(2);
+        expect.assertions(1);
 
         const game = await new Games();
-
+        const review = await new Reviews();
         await game.addNewGame("title", "summary", "desc");
         const retreiveGame = await game.getGameByTitle("title");
-        await expect(addReview(retreiveGame.ID, "Full text", 0))
-            .rejects.toEqual(new Error('Rating must be 1-5'));
+        await expect(review.addReview(retreiveGame.ID, "Full text", 0))
+            .rejects.toEqual(Error('Rating must be 1-5'));
         
         done();
     })
 
     test('Error if: Invalid review _ game does not exist', async done =>{
-        expect.assertions(2);
+        expect.assertions(1);
+       
+        const review = await new Reviews(null);
+        const game = await new Games(null);
+        await expect(review.addReview(0, "Full text", 3))
+            .rejects.toEqual(Error('Game does not exist'));
+        
+        done();
+    })
 
+    test('Error if: Invalid review _ Review is NaN', async done =>{
+        expect.assertions(1);
         const game = await new Games();
+        const review = await new Reviews();
+        await expect(review.addReview(0, "Full text", "string"))
+            .rejects.toEqual(Error('Must supply rating'));
+        
+        done();
+    })
 
-        await game.addNewGame("title", "summary", "desc");
-        const retreiveGame = await game.getGameByTitle("title");
-        await expect(addReview(retreiveGame.ID, "Full text", 3))
-            .rejects.toEqual(new Error('Game "title" does not exist'));
+    test('Error if: Invalid review _ ID is NaN', async done =>{
+        expect.assertions(1);
+        const game = await new Games();
+        const review = await new Reviews();
+        await expect(review.addReview("string", "Full text", 3))
+            .rejects.toEqual(Error('Must supply gameID'));
+        
+        done();
+    })
+
+    test('Error if: Invalid review _ ID is null', async done =>{
+        expect.assertions(1);
+        const game = await new Games();
+        const review = await new Reviews();
+        await expect(review.addReview(null, "Full text", 3))
+            .rejects.toEqual(Error('Must supply gameID'));
         
         done();
     })
