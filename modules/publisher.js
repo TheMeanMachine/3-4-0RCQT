f6d8992fc72818129ab748b88c017755b747de42
machina2 <machina2@coventry.ac.uk>
2019-11-17 17:34:03 +0000

    Minor edit to publisher

--- modules/publisher.js
+++ modules/publisher.js
@@ -7,37 +7,37 @@ const valid = require('./validator');
 const Games = require('./game');
 
 
-module.exports = class Game {
+module.exports = class Publisher {
 
 
     constructor(dbName) {
-    this.validator = new valid();
+        this.validator = new valid();
 
-    return (async() => {
-        this.dbName = dbName || ':memory:';
-        this.db = await sqlite.open(this.dbName);
-        this.games = await new Games(this.dbName);
-        const sql = 
-        [`
-        CREATE TABLE IF NOT EXISTS game_publisher(
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            gameID INTEGER,
-            publisherID INTEGER,
-            FOREIGN KEY (gameID) REFERENCES game(ID),
-            FOREIGN KEY (publisherID) REFERENCES publisher(ID)
-        );`,`
+        return (async() => {
+            this.dbName = dbName || ':memory:';
+            this.db = await sqlite.open(this.dbName);
+            
+            const sql = 
+            [`
+            CREATE TABLE IF NOT EXISTS game_publisher(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                gameID INTEGER,
+                publisherID INTEGER,
+                FOREIGN KEY (gameID) REFERENCES game(ID),
+                FOREIGN KEY (publisherID) REFERENCES publisher(ID)
+            );`,`
 
-        CREATE TABLE IF NOT EXISTS publisher(
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT
-        );`
-        ];
-        for(let i = 0; i < sql.length; i++){
-            await this.db.run(sql[i]);
-        }
-        
-        return this;
-    })()
+            CREATE TABLE IF NOT EXISTS publisher(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT
+            );`
+            ];
+            for(let i = 0; i < sql.length; i++){
+                await this.db.run(sql[i]);
+            }
+            
+            return this;
+        })()
 
     }
 
