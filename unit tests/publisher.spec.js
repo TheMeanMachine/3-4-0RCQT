71b667eac58a14330eece811fb8917b4c268c5c4
machina2 <machina2@coventry.ac.uk>
2019-11-17 15:44:06 +0000

    checkPublisherFields() unit tests

--- unit tests/publisher.spec.js
+++ unit tests/publisher.spec.js
@@ -1,14 +1,14 @@
 
 'use strict'
 
-const Publishers = require('../modules/publisher');
+const Publishers = require('../modules/publisher.js');
 
 
 describe('addPublisher()', ()=>{
     test('Valid publisher', async done =>{
         expect.assertions(1);
         
-        const publisher = new Publishers();
+        const publisher = await new Publishers();
 
         const result = await publisher.addPublisher("Rockstar Games");
         expect(result).toEqual(1);//Returns ID of publisher
@@ -19,11 +19,63 @@ describe('addPublisher()', ()=>{
     test('Error if name is null', async done =>{
         expect.assertions(1);
         
-        const publisher = new Publishers();
+        const publisher =  await new Publishers();
 
         await expect(publisher.addPublisher(null))
             .rejects.toEqual(Error('Must supply name'));
 
+        done();
+    })
+})
+
+describe('checkPublisherFields()', ()=>{
+    test('Valid name', async done =>{
+        expect.assertions(1);
+
+        const publisher = await new Publishers();
+
+        expect(
+            publisher.checkPublisherFields('Rockstar Games')
+            ).toBe(true);
+        done();
+    })
+
+    test('Error if: invalid name', async done =>{
+        expect.assertions(1);
+
+        const publisher = await new Publishers();
+
+        try{
+            publisher.checkPublisherFields('R%(*""@#');
+        }catch(e){
+            expect(e).toEqual(Error('Must supply name'))
+        }
+        done();
+    })
+
+    test('Error if: empty name', async done =>{
+        expect.assertions(1);
+
+        const publisher = await new Publishers();
+
+        try{
+            publisher.checkPublisherFields('');
+        }catch(e){
+            expect(e).toEqual(Error('Must supply name'))
+        }
+        done();
+    })
+
+    test('Error if: null name', async done =>{
+        expect.assertions(1);
+
+        const publisher = await new Publishers();
+
+        try{
+            publisher.checkPublisherFields(null);
+        }catch(e){
+            expect(e).toEqual(Error('Must supply name'))
+        }
         done();
     })
 })
