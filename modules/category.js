4c5919f8546e0bb6c5544a3fdef3d54e6d31e882
machina2 <machina2@coventry.ac.uk>
2019-11-19 19:37:55 +0000

    Template for category class

---
+++ modules/category.js
@@ -0,0 +1,45 @@
+/* eslint-disable max-lines */
+/* eslint-disable max-statements */
+/* eslint-disable complexity */
+/* eslint-disable max-lines-per-function */
+
+'use strict'
+
+const sqlite = require('sqlite-async')
+
+//Custom modules
+const valid = require('./validator')
+
+
+module.exports = class Category {
+	constructor(dbName) {
+		this.validator = new valid()
+
+		return (async() => {
+			this.dbName = dbName || ':memory:'
+			this.db = await sqlite.open(this.dbName)
+
+			const sql =
+            [`
+            CREATE TABLE IF NOT EXISTS game_category(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                gameID INTEGER,
+                categoryID INTEGER,
+                FOREIGN KEY (gameID) REFERENCES game(ID),
+                FOREIGN KEY (categoryID) REFERENCES category(ID)
+            );`,`
+            CREATE TABLE IF NOT EXISTS category(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT
+            );`
+            ]
+			for(let i = 0; i < sql.length; i++) {
+				await this.db.run(sql[i])
+			}
+
+			return this
+		})()
+
+	}
+
+}
