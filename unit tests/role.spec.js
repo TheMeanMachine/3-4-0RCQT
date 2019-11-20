d4e74877114e08702c7dfcfea02794ebbfa2c996
machina2 <machina2@coventry.ac.uk>
2019-11-20 18:37:57 +0000

    getRoleByID() unit tests made

--- unit tests/role.spec.js
+++ unit tests/role.spec.js
@@ -31,3 +31,57 @@ describe('generateRoles', () => {
 		done()
 	})
 })
+
+describe('getRoleByID()', () => {
+	test('Valid roleID', async done => {
+		expect.assertions(1)
+
+		const role = await new Role()
+
+		expect(await role.getRoleByID(1).toMatchObject({
+			ID: 1,
+			role: 'user'
+		}))
+		done()
+	})
+
+	test('Error if roleID null', async done => {
+		expect.assertions(1)
+
+		const role = await new Role()
+
+		await expect( role.getRoleByID(null) )
+			.rejects.toEqual(Error('Must supply roleID'))
+		done()
+	})
+
+	test('Error if roleID undefined', async done => {
+		expect.assertions(1)
+
+		const role = await new Role()
+
+		await expect( role.getRoleByID(undefined) )
+			.rejects.toEqual(Error('Must supply roleID'))
+		done()
+	})
+
+	test('Error if roleID NaN', async done => {
+		expect.assertions(1)
+
+		const role = await new Role()
+
+		await expect( role.getRoleByID('Not a number') )
+			.rejects.toEqual(Error('Must supply roleID'))
+		done()
+	})
+
+	test('Error if role does not exist', async done => {
+		expect.assertions(1)
+
+		const role = await new Role()
+
+		await expect( role.getRoleByID(5) )
+			.rejects.toEqual(Error('Role not found'))
+		done()
+	})
+})
