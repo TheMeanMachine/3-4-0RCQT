24056c82b3f6a93b33499afff7e963615945d12b
machina2 <machina2@coventry.ac.uk>
2019-11-20 17:55:42 +0000

    added generateRoles()

--- unit tests/role.spec.js
+++ unit tests/role.spec.js
@@ -3,96 +3,30 @@
 
 const Role = require('../modules/role.js')
 
-
-describe('associateRole()', () => {
-	test('Valid userID, user', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-		const user = role.user
-
-		const userID = await user.register('Aaron', 'passwordIsNotGood')
-
-		expect(await role.associateRole(1, userID)).toBe(true)
-
-		done()
-	})
-
-	test('Valid userID, admin', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-		const user = role.user
-
-		const userID = await user.register('Aaron', 'passwordIsNotGood')
-
-		expect(await role.associateRole(2, userID)).toBe(true)
-
-		done()
-	})
-
-	test('Error if userID null', async done => {
-		expect.assertions(1)
+describe('generateRoles', () => {
+	test('Admin and user', async done => {
+		expect.assertions(3)
 
 		const role = await new Role()
 
-		await expect(role.associateRole(2, null))
-			.rejects.toEqual(Error('Must supply userID'))
+		expect(await role.generateRoles('user', 'admin'))
+			.toBe(true)
 
-		done()
-	})
+		expect(await role.getRoleByID(1))
+			.toMatchObject(
+				{
+					ID: 1,
+					role: 'user'
+				}
+			)
 
-	test('Error if userID NaN', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-
-		await expect(role.associateRole(2, 'Not a number'))
-			.rejects.toEqual(Error('Must supply userID'))
-
-		done()
-	})
-
-	test('Error if userID undefined', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-
-		await expect(role.associateRole(2, undefined))
-			.rejects.toEqual(Error('Must supply userID'))
-
-		done()
-	})
-
-	test('Error if roleID null', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-
-		await expect(role.associateRole(null, 1))
-			.rejects.toEqual(Error('Must supply roleID'))
-
-		done()
-	})
-
-	test('Error if roleID NaN', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-
-		await expect(role.associateRole('Not a number', 1))
-			.rejects.toEqual(Error('Must supply roleID'))
-
-		done()
-	})
-
-	test('Error if roleID undefined', async done => {
-		expect.assertions(1)
-
-		const role = await new Role()
-
-		await expect(role.associateRole(undefined,1))
-			.rejects.toEqual(Error('Must supply roleID'))
+		expect(await role.getRoleByID(2))
+			.toMatchObject(
+				{
+					ID: 2,
+					role: 'admin'
+				}
+			)
 
 		done()
 	})
