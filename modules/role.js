fd42ee583b6fbe9b6cad6e4579609d002c73b4f3
machina2 <machina2@coventry.ac.uk>
2019-11-20 17:12:06 +0000

    role class template

---
+++ modules/role.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const sqlite = require('sqlite-async')
+
+
+//Custom modules
+const valid = require('./validator')
+const User = require('./user')
+
+module.exports = class role {
+
+	constructor(dbName) {
+		this.validator = new valid()
+		return (async() => {
+			this.dbName = dbName || ':memory:'
+			this.db = await sqlite.open(this.dbName)
+			this.user = await new User(this.dbName)
+			const sql =
+            [`CREATE TABLE IF NOT EXISTS role(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                role TEXT
+            );`,
+            `INSERT INTO role (role)
+            VALUES ("user"),
+            ("admin")`]
+
+			for(let i = 0; i < sql.length; i++) {
+				await this.db.run(sql[i])
+			}
+
+			return this
+		})()
+	}
+
+
+}
