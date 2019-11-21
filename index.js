e0fd9057db6fd863751c0038efb3c324d087ba6a
machina2 <machina2@coventry.ac.uk>
2019-11-21 15:10:04 +0000

    added category bubbles

--- index.js
+++ index.js
@@ -159,6 +159,25 @@ router.get('/game', async ctx => {
 	}
 })
 
+router.post('/addCategoryToGame', async ctx => {
+	try {
+		// extract the data from the request
+		const body = ctx.request.body
+		if(ctx.session.authorised !== true) {//Ensure authorised access
+			return ctx.redirect('/login?msg=you need to log in')
+		}
+
+		const category = await new Category(dbName)
+		const gameID = body.gameID
+		console.log(body.category)
+		await category.associateToCategory(gameID, body.category)
+		//refresh
+		ctx.redirect(`game?gameID=${gameID}`)
+	} catch(err) {
+		await ctx.render('error', {message: err.message})
+	}
+})
+
 /**
  * Script to add a review to a game
  *
