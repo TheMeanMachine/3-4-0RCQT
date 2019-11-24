0c5ba942e4e56035bc2f510b44a167c995e865d8
machina2 <machina2@coventry.ac.uk>
2019-11-24 13:02:38 +0000

    getCommentsByReviewID() unit tests

--- unit tests/comment.spec.js
+++ unit tests/comment.spec.js
@@ -2,6 +2,223 @@
 const Reviews = require('../modules/review.js')
 const Comments = require('../modules/comment.js')
 const Users = require('../modules/user.js')
+const sqlite = require('sqlite-async')
+
+
+describe('deleteCommentByID()', () => {
+	test('Valid comment', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = await review.addReview()
+
+		const fullText = 'I do not think this review is true'
+		const fullText2 = 'Well I do!'
+
+		await comment.addComment(reviewID, userID, fullText)
+		await comment.addComment(reviewID, userID, fullText2)
+		const comments = await comment.getCommentsByReviewID(reviewID)
+
+		expect(comments).toMatchObject(
+			{
+				comments: [{
+					fullText: fullText
+				},
+				{
+					fullText: fullText2
+				}]
+			}
+		)
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if reviewID is null', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = null
+
+		await expect( comment.getCommentsByReviewID(reviewID, userID))
+			.rejects.toEqual(Error('ReviewID must be supplied'))
+
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if reviewID is NaN', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = 'not a number'
+
+		await expect( comment.getCommentsByReviewID(reviewID, userID))
+			.rejects.toEqual(Error('ReviewID must be supplied'))
+
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if reviewID is undefined', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = undefined
+
+		await expect( comment.getCommentsByReviewID(reviewID, userID))
+			.rejects.toEqual(Error('ReviewID must be supplied'))
+
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+})
+
+describe('deleteCommentByID()', () => {
+	test('Valid comment', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = await review.addReview()
+
+		const fullText = 'I do not think this review is true'
+
+		const commentID = await comment.addComment(reviewID, userID, fullText)
+
+		const result = await comment.deleteCommentByID(commentID)
+
+		const comments = await comment.getCommentsByReviewID(reviewID)
+
+		expect(result).toBe(true)
+		expect(comments).toMatchObject(
+			{
+				comments: []
+			}
+		)
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if commentID is undefined', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = await review.addReview()
+
+		const fullText = 'I do not think this review is true'
+
+		const commentID = undefined
+
+		await expect(comment.deleteCommentByID(commentID))
+			.rejects.toEqual(Error('Must supply commentID'))
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if commentID is null', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = await review.addReview()
+
+		const fullText = 'I do not think this review is true'
+
+		const commentID = null
+
+		await expect(comment.deleteCommentByID(commentID))
+			.rejects.toEqual(Error('Must supply commentID'))
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+	test('Error if commentID is NaN', async done => {
+		expect.assertions(1)
+
+		const comment = await new Comments()
+		const review = await new Reviews()
+		const user = await new Users()
+		const userSpy = jest.spyOn(user, 'register').mockImplementation(() => 1)
+		const reviewSpy = jest.spyOn(review, 'addReview').mockImplementation(() => 1)
+
+		const userID = await user.register()
+
+		const reviewID = await review.addReview()
+
+		const fullText = 'I do not think this review is true'
+
+		const commentID = 'not a number'
+
+		await expect(comment.deleteCommentByID(commentID))
+			.rejects.toEqual(Error('Must supply commentID'))
+
+		userSpy.mockRestore()
+		reviewSpy.mockRestore()
+		done()
+	})
+
+})
 
 describe('addComment()', () => {
 	test('Valid comment', async done => {
